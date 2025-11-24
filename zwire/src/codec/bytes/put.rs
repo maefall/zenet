d3a55d4108f7703dc2afe970100e7bd42471f90a
maefall <contact@maefall.com>
2025-11-24 15:29:56 +0100

    use cmp in put_fixed_bytes

--- zwire/src/codec/bytes/put.rs
+++ zwire/src/codec/bytes/put.rs
@@ -2,6 +2,7 @@ use crate::{
     codec::wired::{WiredFixedBytes, WiredInt, WiredIntInner, WiredLengthPrefixed},
     WireError,
 };
+use std::cmp::Ordering;
 use tokio_util::bytes::{BufMut, Bytes, BytesMut};
 
 pub trait BytesMutPutExt {
@@ -24,19 +25,25 @@ impl BytesMutPutExt for BytesMut {
     #[inline]
     fn put_fixed_bytes<B: WiredFixedBytes>(&mut self, payload: &Bytes) -> Result<(), WireError> {
         let payload_length = payload.len();
-        let max_payload_length = B::SIZE;
+        let required_payload_length = B::SIZE;
 
-        if payload_length > max_payload_length {
-            return Err(WireError::Oversized(
+        match payload_length.cmp(&required_payload_length) {
+            Ordering::Greater => Err(WireError::Oversized(
                 B::FIELD_NAME,
                 payload_length,
-                max_payload_length,
-            ));
+                required_payload_length,
+            )),
+            Ordering::Less => Err(WireError::Underflow(
+                B::FIELD_NAME,
+                payload_length,
+                required_payload_length,
+            )),
+            Ordering::Equal => {
+                self.extend_from_slice(payload);
+
+                Ok(())
+            }
         }
-
-        self.extend_from_slice(payload);
-
-        Ok(())
     }
 
     fn put_length_prefixed<I: WiredLengthPrefixed>(
